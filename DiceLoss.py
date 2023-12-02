e7f36777ad5f12bb9275c35ce8fb8a9acb842c25
Siyeol Kim <siyeol@gimsiyeol-ui-MacBookAir.local>
2023-12-03 02:55:53 +0900

    loss, dataset.py 의 padding 수정

--- DiceLoss.py
+++ DiceLoss.py
@@ -1,16 +1,6 @@
 import torch
 import torch.nn as nn
 
-# def dice_loss(output: torch.Tensor, target: torch.Tensor, smooth=1.):
-#     output = output.contiguous()
-#     target = target.contiguous()
-
-#     intersection = (output * target).sum(dim=2).sum(dim=2)
-
-#     loss = (1 - ((2. * intersection + smooth) / (output.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
-
-#     return loss.mean()
-
 class DiceLoss(nn.Module):
     def __init__(self, smooth=1.):
         super(DiceLoss, self).__init__()
@@ -19,7 +9,7 @@ class DiceLoss(nn.Module):
     
     def forward(self, output: torch.Tensor, target: torch.Tensor):
         output = output.contiguous()
-        target = output.contiguous()
+        target = target.contiguous()
 
         intersection = (output * target).sum(dim=2).sum(dim=2)
 
--- loadMAT.py
+++ loadMAT.py
@@ -16,7 +16,32 @@ def loadMAT(path, varname=None):
     else:
         return f[varname]
     
+def padding(data, arraySize):
+    # Make the array the same size as arraySize
+    # Write your code here
+    z, y, x = data.shape
+    print((z, y, x))
+    pad_z = max(0, arraySize - z)
+    pad_y = max(0, arraySize - y)
+    pad_x = max(0, arraySize - x)
+
+    padded_data = np.pad(data, ((0, pad_z), (0, pad_y), (0, pad_x)), mode='constant')
+    return padded_data
+        
 
 if __name__ == '__main__':
-    matFile = loadMAT()
-    
+    matFile = loadMAT('/Users/siyeol/2023-2/software_college_project/train/vertebrae004.mat')
+
+    print(matFile['rawTemp'].shape)
+    temp = np.ascontiguousarray(matFile['rawTemp'])
+
+    padded_temp = padding(temp, arraySize=256)
+
+    print(padded_temp.shape)
+
+    padded_temp = padded_temp.transpose(1, 2, 0)
+
+    print(padded_temp.shape)
+
+    plt.imshow(padded_temp[30])
+    plt.show()
